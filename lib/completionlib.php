6402c40520cbe4f34fabf46bdb2bf68a1dbd9c80
Pedro Jordao <pedrojordaorezende@gmail.com>
2024-12-03 17:10:43 -0300

    MDL-83917 core_completion: Create function count_modules_completed
    The function returns the number of modules completed by a user and
    executes a COUNT aggregate function to avoid running many queries
    to obtain this information, aiming to optimize performance.

--- lib/completionlib.php
+++ lib/completionlib.php
@@ -1641,6 +1641,23 @@ class completion_info {
 
         return (array)$data;
     }
+
+    /**
+     * Return the number of modules completed by a user in one specific course.
+     *
+     * @param int $userid The User ID.
+     * @return int Total number of modules completed by a user
+     */
+    public function count_modules_completed(int $userid): int {
+        global $DB;
+
+        $join = "JOIN {course_modules_completion} cmc ON cm.id=cmc.coursemoduleid";
+        $where = "cm.course=:courseid AND cmc.userid=:userid AND ";
+        $where .= "(cmc.completionstate=" . COMPLETION_COMPLETE . " OR cmc.completionstate=" . COMPLETION_COMPLETE_PASS . ")";
+        $params = ['courseid' => $this->course_id, 'userid' => $userid];
+
+        return $DB->count_records_sql("SELECT COUNT(1) FROM {course_modules} cm {$join} WHERE {$where}", $params);
+    }
 }
 
 /**
